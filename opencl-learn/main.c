82ee34e6670dee5b91c4c90e26093e98c353048a
anancds <anancds@163.com>
2016-07-14 11:56:05 +0800

    Initial Commit

---
+++ opencl-learn/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  opencl-learn
+//
+//  Created by cds on 14/7/16.
+//  Copyright © 2016 cds. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
